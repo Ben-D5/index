e4411b516ba25d72f5cc7f24da99b95fbe5494dc
Ben-D5 <128435309+Ben-D5@users.noreply.github.com>
2023-05-09 18:25:57 -0400

    Update game_2.php

---
+++ game_2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Senior Project</title>
+</head>
+<body>
+<?php include "header.html" ?>
+
+    
+<main class="games">
+
+/* supposed to be a game here, but as you know didnt work out lol */
+
+<br><br>
+
+<img src="tetris.jpg" class="center">
+
+<h3>Instructions: </h3> <p>Control the falling tetrominos with the arrowkeys and press up to rotate them. The goal is to clear as many rows as you can. For every 10 rows cleared, you advance to the next level, with increasing speed of the falling tetrominos.</p>
+    
+</main>
+
+
+</body>
+
+
+<?php include "footer.html" ?>
+<script src="app.js"></script>
+</html>
