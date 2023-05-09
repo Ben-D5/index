98b90a7e78cea5bad9c3d1e8daf7ffe82a7a3eb2
Ben-D5 <128435309+Ben-D5@users.noreply.github.com>
2023-05-09 18:26:57 -0400

    Update index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Senior Project</title>
+</head>
+<body>
+<?php include "header.html" ?>
+
+    
+<main class="games">
+
+/* supposed to be a game here, but as you know didnt work out LMAO */
+<br><br>
+
+  <img src="pong.jpg" class="center">
+
+<h3>Instructions: </h3> <p>Use the arrowkeys to control the paddle. Up key moves the paddle down; vice versa to move the paddle up. The game runs infinitely. The score only keeps track of how many times the AI has scored against you. Have fun!</p>
+    
+</main>
+
+
+</body>
+
+
+<?php include "footer.html" ?>
+<script src="app.js"></script>
+</html>
