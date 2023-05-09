8a7134adfb43e62224f5a21eee6fb46f52e34bf4
Ben-D5 <128435309+Ben-D5@users.noreply.github.com>
2023-05-09 18:24:37 -0400

    Update about_me.php

---
+++ about_me.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Senior Project</title>
+</head>
+<body>
+<?php include "header.html" ?>
+
+    
+<main class="games">
+    <h1 class="title">My CSD Journey So Far</h1>
+  <p class="indent"><br>I've been attending Villa Maria for 4 years whilst only being in the CSD Major for 3 years. I switched to this major after my first year as I thought this would've been a better fitting major. I have acquired decent coding experience from the courses that I took over the years in this major. Started from zero but didn't quite make it to hero. Still better than not have learning anything I suppose.</p>
+
+    
+</main>
+
+
+</body>
+
+
+<?php include "footer.html" ?>
+<script src="app.js"></script>
+</html>
