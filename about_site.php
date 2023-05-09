3c20ffc0ec4a112610cb1250cab28983360489f7
Ben-D5 <128435309+Ben-D5@users.noreply.github.com>
2023-05-09 18:24:03 -0400

    Update about_site.php

---
+++ about_site.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Senior Project</title>
+</head>
+<body>
+<?php include "header.html" ?>
+
+    
+<main class="games">
+    <h1 class="title">The Project Idea</h1>
+  <p class="indent"><br>The idea was to create a website using HTML, CSS, etc., while having the games on it coded in a different language. That language of course being Python. The result is a functioning website compiled up from a markup language and an object-oriented language. However, as you could see, that was hella impossible from the start lol. Really bad foresight on my part. JavaScript would've been the better option.</p>
+
+
+    
+</main>
+
+
+</body>
+
+
+<?php include "footer.html" ?>
+<script src="app.js"></script>
+</html>
